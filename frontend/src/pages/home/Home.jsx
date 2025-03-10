239df04ec4f26ca097ad876cb0a0bc1336394f1e
BalavinayakamD <balavinayakamd.al24@bitsathy.ac.in>
2025-03-10 09:39:01 +0530

    final

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -7,17 +7,17 @@ function Home() {
   return (
     <Box height="100vh" backgroundColor="#00000">
       <Stack direction={"column"} spacing={5} mt={10} mx={10}>
-        <Typography variant="h2" className="text-center ">
+        <Typography variant="h2" className="text-center !important" sx={{fontSize:{xs:"2rem",sm:"2rem",md:"3rem",lg:"4rem",xl:"5rem"}}}>
           Welcome To The Marks Management System !
         </Typography>
-        <Typography variant="h5">
+        <Typography variant="h5" sx={{fontSize:{xs:"1rem",sm:"1rem",md:"1.5rem",lg:"1.5rem",xl:"2rem"}}}>
           Here you can add the marks of your students and manage them.
         </Typography>
-        <Typography variant="h5">
+        <Typography variant="h5" sx={{fontSize:{xs:"1rem",sm:"1rem",md:"1.5rem",lg:"1.5rem",xl:"2rem"}}}>
           This system makes it easy and reliable to manage the marks of your
           students by ensuring Roles.
         </Typography>
-        <Typography variant="h5">
+        <Typography variant="h5"  sx={{fontSize:{xs:"1rem",sm:"1rem",md:"1.5rem",lg:"1.5rem",xl:"2rem"}}}>
           The Roles are decided by your Institution.
         </Typography>
       </Stack>
